33590e4bccb153dc31d509b5dcf8964da0b37ba0
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2024-05-20 17:28:53 +0530

    created a submission

--- script.js
+++
@@ -1,5 +0,0 @@
-//your JS code here. If required.
-<td class="column1">
-				<img class="table-image" src="https://s.yimg.com/ny/api/res/1.2/RZjT6cm5g8ppui2.1E7j8w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://s.yimg.com/os/creatr-uploaded-images/2019-09/731873f0-ddf1-11e9-b353-a68836747aa9">
-				<p class="text">Thomas Michael Shelby OBE DCM MM MP is a fictional character and the protagonist of the British period crime drama Peaky Blinders.</p>
-			</td>
